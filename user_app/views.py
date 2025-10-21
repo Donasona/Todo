46e326f916b484976ded88805bbd08d5c1c2eb45
DONA PAUL <donapaul901@gmail.com>
2025-10-21 14:54:58 +0530

    cmt

---
+++ user_app/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import View
+
+from user_app.forms import Userregisterform
+
+from user_app.models import User
+
+from django.contrib.auth import authenticate,login
+
+
+class Registerview(View):
+    def get(self,request):
+        form =Userregisterform()
+        return render(request,"signup.html",{'form':form})
+
+    def post(self,request):
+        print(request.POST)
+        username =request.POST.get('username')
+
+        first_name =request.POST.get('first_name')
+
+        last_name =request.POST.get('last_name')
+
+        password =request.POST.get('password')  
+
+        email =request.POST.get('email')
+
+        User.objects.create_user(username=username,
+                                 first_name=first_name,
+                                 last_name=last_name,
+                                 password=password,
+                                 email=email)
+        form = Userregisterform()
+        return render(request,'signup.html',{'form':form})
+
+class LoginView(View):
+    def get(self,request):
+
+        return render(request,'signin.html')
+
+    def post(self,request):
+        username =request.POST.get('username')
+        password =request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+        if user:
+            login(request,user)
+            return render(request,"signup.html")
+        return render(request,"signin.html")
+
+
+
+
