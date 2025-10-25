b322011ca7623ff82df22b0843b47935cd7df042
DONA PAUL <donapaul901@gmail.com>
2025-10-25 19:22:02 +0530

    forth

--- user_app/forms.py
+++ user_app/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from user_app.models import User
+from user_app.models import Task
 
 class Userregisterform(forms.ModelForm):
     class Meta:
         model = User
         fields =['username','first_name','last_name','password','email']
+
+class Taskform(forms.ModelForm):
+    class Meta:
+        model =Task
+        fields =['title']
+        
