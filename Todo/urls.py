1a4ae2349ae64c28935a615ddabf81aa25ca2003
DONA PAUL <donapaul901@gmail.com>
2025-10-26 21:15:07 +0530

    ten

--- Todo/urls.py
+++ Todo/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from user_app.views import Registerview,LoginView,LogoutView,TaskView
+from task_app.views import Add_task_view
 
 
 urlpatterns = [
@@ -25,4 +26,5 @@ urlpatterns = [
     path('signin/',LoginView.as_view(),name="login"),
     path('logout/',LogoutView.as_view(),name="logout"),
     path('taskview/',TaskView.as_view()),
+    path('addtask/',Add_task_view.as_view()),
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
-from task_app.forms import Taskform
+from task_app.forms import TaskForm
 # Create your views here.
 class Add_task_view(View):
     def get(self,request):
-        form = Taskform()
+        form = TaskForm()
         return render(request,"task_add.html",{"form":form})
+    
+    def post(self,request):
+        print(request.POST)
+        form =TaskForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            task =form.save(commit=False)
+            task.user =request.user
+            task.save()
+        return render(request,"task_add.html",{"form":form})    
