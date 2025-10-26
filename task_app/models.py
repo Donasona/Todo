23f08461d8d8d96f73a025448899923d913fb176
DONA PAUL <donapaul901@gmail.com>
2025-10-26 21:18:15 +0530

    tweleve

--- task_app/models.py
+++ task_app/models.py
@@ -3,3 +3,13 @@ from django.db import models
 from user_app.models import User
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    task = models.CharField(max_length=100)
+
+    created_date = models.DateTimeField(auto_now_add=True)  
+
+    is_completed = models.BooleanField(default=False)
+
+    priority = models.Choices("high","low") 
